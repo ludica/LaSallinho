df3bf5a504570e0f9efaa69684002aa5521449bf
Vitor Garcia <vpgarcia17@gmail.com>
2014-12-15 21:27:32 -0200

    Alteração das imagem e tamanho dos inimigos

--- core/src/com/mygdx/game/Inimigo.java
+++ core/src/com/mygdx/game/Inimigo.java
@@ -17,9 +17,9 @@ public class Inimigo {
 	public float largura = 0;
 
 	public Inimigo(float x, float y, float velocidade) {
-		this.texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+		this.texture = new Texture(Gdx.files.internal("enemies/slime.png"));
 		this.image = new Sprite(texture);
-		image.setSize(20, 20);
+		image.setSize(50f/28f*50f, 50f);
 		this.bounds = new Rectangle(x, y, image.getWidth(), image.getHeight());
 		this.velocidade = velocidade;
 	}
@@ -27,11 +27,11 @@ public class Inimigo {
 	public void mover(TiledMapTileLayer layerCollision) {
 		if(direcao > 0){
 			largura = bounds.width;
-			if(image.isFlipX()) image.flip(true, false);
+			if(!image.isFlipX()) image.flip(true, false);
 		}
 		if(direcao < 0){
 			largura = 0;
-			if(!image.isFlipX()) image.flip(true, false);
+			if(image.isFlipX()) image.flip(true, false);
 		}
 		ArrayList<Rectangle> parede = new ArrayList<Rectangle>();
 		parede.clear();
