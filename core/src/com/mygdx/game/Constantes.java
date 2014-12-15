2958bf7d7dd97d91493909e2d21353125c6a6e30
Vitor Garcia <vpgarcia17@gmail.com>
2014-12-15 21:25:55 -0200

    Atualização das Constantes para novo mapa

--- core/src/com/mygdx/game/Constantes.java
+++ core/src/com/mygdx/game/Constantes.java
@@ -2,19 +2,20 @@ package com.mygdx.game;
 
 public class Constantes {
 
-	public static final int TILESCALE = 16;
+	public static final int TILESCALE = 70;
 	public static final int TILESHORIZONTAL = 200;
 	public static final int TILESVERTICAL = 32;
-	public static final int LARGURA_TELA = 275;
-	public static final int ALTURA_TELA = 138;
+	public static final int LARGURA_TELA = TILESCALE * 20;
+	public static final int ALTURA_TELA = TILESCALE * 10;
 	
 	public static final int X_INICIAL = TILESCALE * 11;
 	public static final int Y_INICIAL = TILESCALE * 17;
 	
-	public static final float GRAVIDADE = 4.5f;
-	public static final float UPFORCE = GRAVIDADE + 3.5f;
-	public static final float VELOCIDADE = 3.0f;
-	public static final float MAXJUMP = 80;
+	public static final float GRAVIDADE = 12f;
+	public static final float UPFORCE = GRAVIDADE + 10f;
+	public static final float VELOCIDADE = 10f;
+        
+	public static final float MAXJUMP = TILESCALE * 4.5f;
 	
 	public static final int IDITEM1 = 1;
 	public static final int IDITEM2 = 2;
