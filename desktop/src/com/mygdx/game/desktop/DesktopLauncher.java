a2134962d45c4bda7a2c7da265ef018c5bea3c24
Vitor Garcia <vpgarcia17@gmail.com>
2014-12-15 21:20:36 -0200

    Tamanho da Tela no PC

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,13 +3,14 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.LaSalinhoGame;
+import com.mygdx.game.Constantes;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "La Salinho";
-		//config.width = 800;
-		//config.height = 400;
+		config.width = Constantes.LARGURA_TELA/2;
+		config.height = Constantes.ALTURA_TELA/2;
 		config.resizable = false;
 		new LwjglApplication(new LaSalinhoGame(), config);
 	}
