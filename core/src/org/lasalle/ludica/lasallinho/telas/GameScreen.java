36c7d3a0b575ee102123d75d9de310d0cde4a151
Felipe Brito <felipebritor@gmail.com>
2014-12-15 22:51:29 -0200

    "Pause" Pós Perda de Vida
    Gambiarra para criar efeito de "pause" para quando o personagem perder
    uma vida. Feio, eu sei

--- core/src/org/lasalle/ludica/lasallinho/telas/GameScreen.java
+++ core/src/org/lasalle/ludica/lasallinho/telas/GameScreen.java
@@ -8,6 +8,7 @@ import org.lasalle.ludica.lasallinho.fabricas.ItemFactory;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -70,7 +71,7 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1F, 1F, 1F, 1F);
+		Gdx.gl.glClearColor(85/255f,170/229f,255/255f, 0.8f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		laSalinho.Mover(layerCollision);
@@ -85,7 +86,7 @@ public class GameScreen implements Screen {
 		batch.setProjectionMatrix(camera.combined);
 		enquadrarCamera(laSalinho.bounds);
 		
-		laSalinho.stateTime += delta;
+		laSalinho.setStateTime(laSalinho.getStateTime() + delta);
 		TextureRegion frame = laSalinho.Imagem();
 		
 		if(LaSalinho.isDireita){
@@ -112,6 +113,14 @@ public class GameScreen implements Screen {
 			inimigo.gravidade(layerCollision);
 			inimigo.mover(layerCollision);
 			if(laSalinho.InimigoColide(inimigo.bounds)){
+				try {
+					Sound som = Gdx.audio.newSound(Gdx.files.internal("sons/morte.ogg"));
+					som.play();
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				LaSalinho.PerdeVida();
 				laSalinho = new LaSalinho();
 			}
